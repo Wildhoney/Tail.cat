18fba8719b80ae7912fa3e4197e61520d5167f8e
Adam Timberlake <adam.timberlake@gmail.com>
2013-08-28 01:15:49 +0100

    Extended IMAP configuration. Relates to #1

--- server/node.js
+++ server/node.js
@@ -1,4 +1,45 @@
+// Configure Express and any dependencies, such as the IMAP module for Node.js.
 var express = require('express'),
-    app     = express();
+    app     = express(),
+    Imap    = require('imap'),
+    inspect = require('util').inspect;
+
+// Attempt to make a connection to the specified IMAP server.
+var imap = new Imap({
+    user        : 'adam.timberlake+tailcat@gmail.com',
+    password    : '...',
+    host        : 'imap.gmail.com',
+    port        : 993,
+    tls         : true,
+    tlsOptions  : { rejectUnauthorized: false }
+});
+
+imap.connect(function connect(error) {
+
+    /**
+     * @method exit
+     * @return {Boolean}
+     */
+    var exit = function exit() {
+        console.error(error);
+        return false;
+    };
+
+    if (error) {
+        // An error occurred, I'm afraid!
+        return exit();
+    }
+
+    // Open the 'INBOX' on the IMAP server.
+    imap.openBox('INBOX', false, function(error, box) {
+
+        if (error) {
+            // An error occurred, I'm afraid!
+            return exit();
+        }
+
+    });
+
+});
 
 app.listen(2150);
